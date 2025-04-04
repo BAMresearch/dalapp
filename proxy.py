e487d41234082e875f8dc2470c4af1c09e4cd5cb
Ingo Breßler <ingo.bressler@bam.de>
2025-04-04 20:11:37 +0200

    enh(proxy): automatically replace server addr in html file, flask app via factory method

--- proxy.py
+++ proxy.py
@@ -11,41 +11,63 @@ config = dict( # defaults, order of cmdline arguments
     targetURL = "https://bam-openbis02.germanywestcentral.cloudapp.azure.com",
     webPort = "8000",
 )
-app = Flask(__name__)
 
-# parse args
-for i, key in enumerate(config.keys()):
-    try:
-        config[key] = sys.argv[i+1]
-    except:
-        pass
+def parse_args():
+    for i, key in enumerate(config.keys()):
+        try:
+            config[key] = sys.argv[i+1]
+        except:
+            pass
 
-print(f"Using the following config: ")
-pprint.pprint(config)
-print(f"Run the web server with:\n"
-        f"    python3 -m http.server -b {config["hostAddr"]} {config["webPort"]}\n")
+    print(f"Using the following config: ")
+    pprint.pprint(config)
+    print(f"Run the web server with:\n"
+            f"    python3 -m http.server -b {config["hostAddr"]} {config["webPort"]}\n")
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
+# change server address to match the proxy address here
+def update_index():
+    fn = "index.html"
+    with open(fn) as fd:
+        content = fd.read()
+    content = content.replace("127.0.0.1:5000",
+                              f"{config["hostAddr"]}:{config["hostPort"]}")
+    with open(fn, 'w') as fd:
+        fd.write(content)
 
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
-def proxy(path):
-    url = f'{config["targetURL"]}/{path}'  # Replace with your target URL
-    headers = dict(request.headers)
-    if 'Host' in headers:
-        del headers['Host']
-    response = requests.request(
-        method=request.method,
-        url=url,
-        headers=headers,
-        data=request.get_data(),
-        allow_redirects=True
-    )
-    return response.content, response.status_code
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize your model or run custom function here
+    parse_args()
+    update_index()
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,append,delete,entries,foreach,get,has,keys,set,values')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+    def proxy(path):
+        url = f'{config["targetURL"]}/{path}'  # Replace with your target URL
+        headers = dict(request.headers)
+        if 'Host' in headers:
+            del headers['Host']
+        requestArgs = dict(
+            method=request.method,
+            url=url,
+            headers=headers,
+            data=request.get_data(),
+            allow_redirects=True,
+            verify=True) # change this to False for selfsigned certs
+        response = requests.request(**requestArgs)
+        return response.content, response.status_code
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True, host=config["hostAddr"], port=config["hostPort"])
+    app = create_app()
+    app.run(debug=True, use_reloader=False,
+            host=config["hostAddr"], port=config["hostPort"])
